fba6a8b86b1089c10ac939622d8e01a78a88a619
ziv hayut <zivh113@gmail.com>
2025-06-11 16:11:05 +0300

    mega prank

--- Public/main.js
+++ Public/main.js
@@ -3,3 +3,11 @@ function sayHello() {
   alert("Youre Gay AF Boy " + "\nyour man bodycount: " + count);
   count++;
 }
+
+function sayFaggot() {
+  alert(
+    "Sike Faggot!!!" +
+      "\nyour man bodycount: 1000" +
+      "\nto go to the real home page press under the fake one!"
+  );
+}
